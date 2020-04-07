9ada07b554d59c3f21dfe082a0d8f8a31b21f23d
Trongnv4 <v.trongnv4@vinid.net>
2020-04-07 15:44:38 +0700

    add views path

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const express = require('express');
+const sendMail = require('./mailer');
 // console.log(__dirname);
 // console.log(path.join(__dirname, '../public'));
 const app = express();
 const publicDirectory = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../views');
 
 app.use(express.static(publicDirectory)); // use index.html static file as homepage
 app.set('view engine', 'hbs'); // set views folder as default view
+app.set('views', viewPath);
 
 app.get('', (req, res) => {
   res.render('index', { 
